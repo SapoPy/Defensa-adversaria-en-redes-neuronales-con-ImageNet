e39121be716adefe926ca8e8edadd2d056b787c0
SapoPy <benjamin.alfonso.wulf@gmail.com>
2025-10-08 22:22:31 -0300

    Mueve los modelos a la carpeta Modelos

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from ImageNet100ValDataset import *
-from ResNet50 import evaluate_resnet50
-from ResNet18 import evaluate_resnet18
+from Modelos.ResNet50 import evaluate_resnet50
+from Modelos.ResNet18 import evaluate_resnet18
 
 if __name__ == "__main__":
     print(f"Precisión ResNet50 en validación: {evaluate_resnet50():.4f}")
